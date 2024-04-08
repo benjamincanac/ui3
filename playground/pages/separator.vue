d0da790ef001e052154c9a39b291faa81e3ce197
Sandros94 <sandro.circi@digitoolmedia.com>
2024-04-08 17:54:18 +0200

    chore(Separator): update playground

--- playground/pages/separator.vue
+++ playground/pages/separator.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-full max-w-xs mx-4">
     <div class="text-white leading-5 font-semibold">
-      Radix Primitives
+      Nuxt UI
     </div>
     <div class="text-white leading-5">
       An open-source UI component library.
     </div>
     <USeparator icon="i-simple-icons-nuxtdotjs" type="dashed" />
-    <div class="flex h-24 items-center">
+    <div class="h-24 flex gap-4 items-center">
       <div class="text-white leading-5">
         Blog
       </div>
