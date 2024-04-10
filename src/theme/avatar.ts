b3db12873e4fca8bee87e97cdd2aa6accb5b0eb2
Sandros94 <sandro.circi@digitoolmedia.com>
2024-04-10 20:05:52 +0200

    feat(Avatar): handle font size based on container queries

--- src/theme/avatar.ts
+++ src/theme/avatar.ts
@@ -1,38 +1,38 @@
 export default {
   slots: {
-    root: 'inline-flex items-center justify-center shrink-0 select-none overflow-hidden rounded-full align-middle bg-gray-100 dark:bg-gray-800',
-    image: 'h-full w-full rounded-[inherit] object-cover',
-    fallback: 'font-medium leading-none text-gray-500 dark:text-gray-400 truncate',
-    icon: 'text-gray-500 dark:text-gray-400 shrink-0'
+    root: '@container/avatar inline-flex items-center justify-center shrink-0 select-none overflow-hidden rounded-full align-middle bg-gray-100 dark:bg-gray-800',
+    image: 'text-[50cqi] h-full w-full rounded-[inherit] object-cover',
+    fallback: 'text-[50cqi] font-medium leading-none text-gray-500 dark:text-gray-400 truncate',
+    icon: 'text-[50cqi] text-gray-500 dark:text-gray-400 shrink-0'
   },
   variants: {
     size: {
       '3xs': {
-        root: 'size-4 text-[8px]'
+        root: 'size-4'
       },
       '2xs': {
-        root: 'size-5 text-[10px]'
+        root: 'size-5'
       },
       xs: {
-        root: 'size-6 text-xs'
+        root: 'size-6'
       },
       sm: {
-        root: 'size-7 text-sm'
+        root: 'size-7'
       },
       md: {
-        root: 'size-8 text-base'
+        root: 'size-8'
       },
       lg: {
-        root: 'size-9 text-lg'
+        root: 'size-9'
       },
       xl: {
-        root: 'size-10 text-xl'
+        root: 'size-10'
       },
       '2xl': {
-        root: 'size-11 text-[22px]'
+        root: 'size-11'
       },
       '3xl': {
-        root: 'size-12 text-2xl'
+        root: 'size-12'
       }
     }
   },
